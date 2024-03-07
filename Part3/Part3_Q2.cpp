c855529cc4c55cafb10b6f4a875f9e75a8fa1fee
erikazhengyilin <zhengyil@msu.edu>
2024-03-07 13:12:04 -0500

    p3 results

--- Part3/Part3_Q2.cpp
+++ Part3/Part3_Q2.cpp
@@ -22,7 +22,8 @@ int main(int argc, char *argv[]) {
 
     int send_to = (world_rank + 1) % world_size;
     int recv_from = (world_rank - 1 + world_size) % world_size;
-
+    std::ofstream myfile;
+    myfile.open("output.csv", ios::out | ios::app);
     double start_time, end_time;
 
     MPI_Barrier(MPI_COMM_WORLD);
@@ -35,22 +36,20 @@ int main(int argc, char *argv[]) {
     MPI_Barrier(MPI_COMM_WORLD);
     end_time = MPI_Wtime();
 
-    for (int i = 0; i < world_size; ++i) {
-        MPI_Barrier(MPI_COMM_WORLD); 
-        if (world_rank == i) {
+    if (world_rank == 0) {
             cout << "Message Size: " << message_size << " bytes" << endl;
             cout << "Processes: " << world_size << endl;
-            cout << "Processe number : " << world_rank << endl;
+            // cout << "Processe number : " << world_rank << endl;
             cout << "Time Taken: " << (end_time - start_time) << " seconds" << endl;
 
-            double bandwidth = message_size / (end_time - start_time);
-            double latency = (end_time - start_time) / world_size;
+            // double bandwidth = message_size / (end_time - start_time);
+            // double latency = (end_time - start_time) / world_size;
 
-            cout << "Bandwidth: " << bandwidth << " bytes/second" << endl;
-            cout << "Latency: " << latency << " seconds" << endl;
+            // cout << "Bandwidth: " << bandwidth << " bytes/second" << endl;
+            // cout << "Latency: " << latency << " seconds" << endl;
             cout << endl;
+            myfile << message_size << "," << world_size << "," << (end_time - start_time) << "\n";
         }
-    }
 
     delete[] message;
     delete[] recv_buffer;
